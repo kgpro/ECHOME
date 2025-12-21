39b21c8987cdd774448f2d83a6edac5eb8985602
kamlesh gujrati <gujratikamleshanil@gmail.com>
2025-12-21 21:56:30 +0530

    Update DEBUG variable assignment in settings.py
    Changed DEBUG environment variable retrieval to keep it as a boolean.

--- ECHOME/settings.py
+++ ECHOME/settings.py
@@ -18,7 +18,7 @@ load_dotenv(dotenv_path=env_path, override=True)
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-default-key")
 
 # Debug mode
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "False") 
 
 
 # Allowed hosts
